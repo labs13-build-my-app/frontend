36ed258be855607d7d2326d4348f0ef7d080424b
deanovision <rodeanfraser@gmail.com>
2019-06-05 13:29:09 -0400

    updated callback component and Auth

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import User from "./components/User";
 import Auth from "./components/Auth/Auth";
-import { Route } from "react-router";
+import { Route, Link } from "react-router-dom";
 import "./App.css";
 import Callback from "./components/Auth/Callback";
 import Authenticated from "./components/Auth/Authenticated";
@@ -12,8 +12,18 @@ function App() {
   return (
     <div className="App">
       {/* <User /> */}
-      <button onClick={() => auth.login()}>Login Here</button>
+      <Link to="/login">
+        <button>Login Here</button>
+      </Link>
+
       <button onClick={() => auth.logout()}>Log Out Here</button>
+      <Route
+        exact
+        path="/login"
+        render={() => {
+          auth.login();
+        }}
+      />
       <Route exact path="/callback" component={Callback} />
       <Route path="/authenticated" component={Authenticated} />
     </div>
---
+++ src/components/Login.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Auth from "./Auth/Auth";
+
+const Login = () => {
+  //   const auth = new Auth();
+  //   auth.login();
+  return <div>Login</div>;
+};
+export default Login;
