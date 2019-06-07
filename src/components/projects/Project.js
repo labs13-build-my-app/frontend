987653e29b22a84e44f22a1eac49efb219dc9722
Arkadiusz Kusiak <akusiak23@gmail.com>
2019-06-07 12:29:56 -0500

    projects components

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -1,15 +1,28 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const Project = () => {
-  const [project, setProject] = useState([]);
-  useEffect(() => {
-    axios
-      .get("https://build-my-app.herokuapp.comapi/projects/projects/project")
-      .then(res => {
-        setProjects(res.data.projects);
-      });
-  }, []);
+const Project = props => {
+  // const [project, setProject] = useState([]);
+  // useEffect(() => {
+  //   axios
+  //     .get("http://localhost:8000/api/projects/project/:id")
+  //     .then(res => {
+  //       setProjects(res.data.projects);
+  //     });
+  // }, []);
+
+  return (
+    <div className="Projects">
+      <div className="Book-Top">
+        <h3 className="ProjectTitle">{props.name}</h3>
+      </div>
+      <div>
+        <p>{props.description}</p>
+        <p>{props.budget}</p>
+        <p>{props.dueDate}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Project;
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,22 +1,32 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Project from "./Project";
+import { Link } from "react-router-dom";
 
 const Projects = () => {
   const [projects, setProjects] = useState([]);
   useEffect(() => {
     axios.get("http://localhost:8000/api/projects/").then(res => {
       setProjects(res.data);
+      console.log(res.data);
     });
   }, []);
   return (
     <div>
       {projects.map(project => (
-        <div key={project.id}>
-          <p>{project.name}</p>
-          <p>{project.description}</p>
-          <p>{project.budget}</p>
-          <p>{project.dueDate}</p>
-        </div>
+        <Link
+          className="project-link"
+          to={`/project/${project.id}`}
+          key={project.id}
+        >
+          <Project
+            name={project.name}
+            description={project.description}
+            budget={project.budget}
+            dueDate={project.dueDate}
+            key={project.id}
+          />
+        </Link>
       ))}
     </div>
   );
