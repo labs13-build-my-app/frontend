b36e44b0bcdd0bfcbfebf1d7b2e27ad1ee04f2f8
Arkadiusz Kusiak <akusiak23@gmail.com>
2019-06-14 10:30:05 -0500

    Plan styling and functionality

--- src/components/projects/Plan.js
+++ src/components/projects/Plan.js
@@ -1,11 +1,57 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { fetchPlan } from "../../store/actions";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import { makeStyles } from "@material-ui/core/styles";
+import { Button } from "../../custom-styles";
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1)
+  },
+  dense: {
+    marginTop: theme.spacing(2)
+  },
+  root: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2)
+  }
+}));
 
 const Plan = ({ match, isLoading, isSignedIn, role }) => {
+  const { fullScreen } = props;
+  const [open, setOpen] = React.useState(false);
+  const classes = useStyles();
   const [plan, setPlan] = useState([]);
+  const [planStatus, setPlanStatus] = useState([]);
 
-  console.log(match);
+  function handleClickOpen() {
+    setOpen(true);
+  }
+
+  function handleClose() {
+    setOpen(false);
+  }
+
+  const changeHandler = (e, setState) => {
+    let user = e.target.value;
+    setState(user);
+  };
 
   useEffect(() => {
     fetchPlan(match.params.plan_id)(setPlan);
@@ -18,6 +64,20 @@ const Plan = ({ match, isLoading, isSignedIn, role }) => {
       <h1>bud: {plan.budget}</h1>
       <h1>date: {plan.dueDate}</h1>
       <h1>status: {plan.planStatus}</h1>
+
+      <FormControl className={classes.formControl}>
+        <InputLabel>Plan Status</InputLabel>
+        <Select
+          className={classes.select}
+          value={role}
+          onChange={event => changeHandler(event, setPlanStatus)}
+        >
+          <MenuItem value="" />
+          <MenuItem value={"In Progress"}>In Progress</MenuItem>
+          <MenuItem value={"Completed"}>Completed</MenuItem>
+        </Select>
+      </FormControl>
+      <Button type="submit">Submit</Button>
     </div>
   );
 };
