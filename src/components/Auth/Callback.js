381b40d3e9e48ba18443d9d517c42d8418216fbe
joe-alfaro <josephlalfaro@gmail.com>
2019-06-21 14:27:51 -0700

    change rul sources depending on environment variables

--- src/components/Auth/Callback.js
+++ src/components/Auth/Callback.js
@@ -4,7 +4,9 @@ import auth0 from "auth0-js";
 const auth = new auth0.WebAuth({
   domain: "dev-juy4gqyj.auth0.com",
   clientID: "erkAAAar4RrEqx4GcMSefhL42s2fulSu",
-  redirectUri: "http://localhost:3000/callback",
+  redirectUri: process.env.NODE_ENV === 'production' 
+    ? 'https://build-my-app-fe.onrender.com/callback' 
+    : 'http://localhost:3000/callback',
   responseType: "token id_token",
   scope: "openid profile"
 });
