1355aed82697313194d8e40b91da0e8e8ba48eeb
joe-alfaro <josephlalfaro@gmail.com>
2019-06-26 09:39:16 -0700

    add filtering for the list of developers

--- src/components/profiles/DeveloperList.js
+++ src/components/profiles/DeveloperList.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 import { deepPurple } from "@material-ui/core/colors";
 import Grid from "@material-ui/core/Grid";
 import Divider from "@material-ui/core/Divider";
@@ -8,8 +8,11 @@ import Paper from "@material-ui/core/Paper";
 import Card from "@material-ui/core/Card";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
-import { Pill } from "../../custom-styles";
+import { Pill, PageTitle } from "../../custom-styles";
 import { fetchDevelopers } from "../../store/actions";
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
 const Developers = ({ history }) => {
   const useStyles = makeStyles(theme => ({
@@ -77,23 +80,50 @@ const Developers = ({ history }) => {
     }
   }));
   const classes = useStyles();
+  
+  const theme = useTheme();
+  const [value, setValue] = useState(0);
+  const [filter, setFilter] = useState('');
+  const filters = ['all', 'web', 'ios' ,'android'];
+
+  useEffect(()=>{
+    setFilter(filters[value]);
+  }, [value]);
+
+  function handleChange(event, newValue) {
+    setValue(newValue);
+  }
 
   const [developers, setDevelopers] = useState([]);
   const [page, setPage] = useState({});
   useEffect(() => {
     fetchDevelopers(setDevelopers, setPage);
   }, []);
+
   if (developers.length === 0) {
     return <h1>Loading...</h1>;
   } else {
     return (
       <>
-        <h2>Available Developers</h2>
+        <PageTitle style={{width: '100%', paddingLeft:'4%'}}>Available Developers</PageTitle>
+        <AppBar position="static" color="default">
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            indicatorColor="primary"
+            textColor="primary"
+            variant="fullWidth"
+          >
+            <Tab label="All" />
+            <Tab label="Web" />
+            <Tab label="iOS" />
+            <Tab label="Android" />
+          </Tabs>
+        </AppBar>
         <div className={classes.divContainer} style={{ width: "100%" }}>
-          {developers.map(dev => {
-            const initials = dev.firstName.charAt(0) + dev.lastName.charAt(0);
-            return (
-              <div className={classes.cardContainer} key={dev.id}>
+          {developers.map(dev => (
+            (dev.devType.toLowerCase() === filter || filter === 'all') 
+              ? (<div className={classes.cardContainer} key={dev.id}>
                 <Card
                   className={classes.root}
                   onClick={() => history.push(`/profile/${dev.id}`)}
@@ -111,7 +141,7 @@ const Developers = ({ history }) => {
                         alt={dev.firstName}
                         className={classes.purpleAvatar}
                       >
-                        {initials}
+                        {dev.firstName.charAt(0) + dev.lastName.charAt(0)}
                       </Avatar>
                     )}
                     <CardHeader
@@ -122,9 +152,9 @@ const Developers = ({ history }) => {
                   <Divider variant="middle" />
                   <p>Skills: {dev.skills}</p>
                 </Card>
-              </div>
-            );
-          })}
+              </div>)
+            :null
+          ))}
         </div>
       </>
     );
