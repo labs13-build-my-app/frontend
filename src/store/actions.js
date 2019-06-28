b640b194275c348ca1f9a0bf81cf492a36509e09
deanovision <rodeanfraser@gmail.com>
2019-06-27 23:09:12 -0400

    setup homepage component

--- src/store/actions.js
+++ src/store/actions.js
@@ -41,11 +41,9 @@ const local = "http://localhost:8000";
 const connection = process.env.NODE_ENV === "development" ? local : heroku;
 
 export const formatDate = (date = "") => {
+  date = String(date);
   date = date.includes("Z") ? date.slice(0, -1) : date;
-  date =
-    date.includes(".") && process.env.NODE_ENV === "development"
-      ? new Date(Number(date))
-      : date;
+  date = process.env.NODE_ENV === "development" ? new Date(Number(date)) : date;
 
   const someDate =
     process.env.NODE_ENV === "development"
@@ -361,6 +359,13 @@ export const fetchDeveloperPlans = (developer_id, dispatch) => {
     url: `${connection}/api/projects/plan-list-developer/${developer_id}`
   })
     .then(res => {
+      // const formattedData = res.data.map(plan => {
+      //   return {
+      //     ...plan,
+      //     dueDate: moment(plan.dueDate).format("MMMM DD YYYY")
+      //   };
+      // });
+      // console.log(formattedData);
       res.data.message === "No Plans" ? dispatch([]) : dispatch(res.data);
     })
     .catch(error => {
