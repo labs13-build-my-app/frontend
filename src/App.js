3eb46a257a7b4f3af45eb6caa2468f4814c71657
nomadic4life <orbison.dennis@gmail.com>
2019-07-03 00:57:36 -0700

    refactored ProfileContainer to accept a Component as a prop. the component prop will be for developer and project owner and maybe admin

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { useEffect, useReducer, useState } from "react";
 import { withRouter, Route } from "react-router";
 import ProfileContainer from "./newApp/ProfileContainer";
+import ProfileCard from "./newApp/ProfileCard";
 
 const App = () => {
   return (
     <div className="App">
       <Route
         path="/profile/:user_id"
-        render={props => <ProfileContainer id={props.match.params.user_id} />}
+        render={props => (
+          <ProfileContainer
+            id={props.match.params.user_id}
+            component={ProfileCard}
+          />
+        )}
       />
       {/* <ProfileContainer /> */}
     </div>
--- src/newApp/ProfileContainer.js
+++ src/newApp/ProfileContainer.js
@@ -1,19 +1,9 @@
 import React from "react";
 import Profile from "./ProfileView";
-import ProfileCard from "./ProfileCard";
 
-const ProfileContainer = ({ id }) => {
-  return (
-    <Profile id={id}>
-      {user => {
-        return (
-          <>
-            <ProfileCard user={user} />
-          </>
-        );
-      }}
-    </Profile>
-  );
+const ProfileContainer = ({ id, component }) => {
+  const Component = component;
+  return <Profile id={id}>{user => <Component user={user} />}</Profile>;
 };
 
 export default ProfileContainer;
