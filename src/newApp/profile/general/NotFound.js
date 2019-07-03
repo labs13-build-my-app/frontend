bacb5108d43c13c47df612408b880511488c02f7
nomadic4life <orbison.dennis@gmail.com>
2019-07-03 04:40:30 -0700

    created not found component for incorrect id type

---
+++ src/newApp/profile/general/NotFound.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NotFound = () => {
+  return <div>incorrect given ID, please provide a user ID</div>;
+};
+
+export default NotFound;
--- src/newApp/profile/general/ProfileContainer.js
+++ src/newApp/profile/general/ProfileContainer.js
@@ -1,11 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Profile from "./ProfileView";
+import NotFound from "./NotFound";
 
 const ProfileContainer = ({ id, component }) => {
   const ProfileView = component;
   if (isNaN(id)) {
-    return <div> 404 component not found </div>;
+    return <NotFound />;
   }
   return <Profile id={id}>{user => <ProfileView user={user} />}</Profile>;
 };
