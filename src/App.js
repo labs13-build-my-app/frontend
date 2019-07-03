137730f34a4e04db87439d0950c58667627c5a9b
nomadic4life <orbison.dennis@gmail.com>
2019-07-03 04:05:27 -0700

    move routes into Route Container, changed path name for profile

--- src/App.js
+++ src/App.js
@@ -1,39 +1,11 @@
 import React from "react";
 import { withRouter, Route } from "react-router";
-import ProfileContainer from "./newApp/profile/general/ProfileContainer";
-import {
-  DeveloperPage,
-  ProjectOwnerPage,
-  AdminPage
-} from "./newApp/profile/roles";
+import RouteContainer from "./newApp/RouteContainer";
 
 const App = () => {
   return (
     <div className="App">
-      <Route
-        path="/profile/:type/:user_id"
-        render={props => {
-          let component = null;
-          const { type } = props.match.params;
-          if (type === "developer") {
-            component = DeveloperPage;
-          } else if (type === "project-owner") {
-            component = ProjectOwnerPage;
-          } else if (type === "admin") {
-            // redirect?
-            component = AdminPage;
-          } else {
-            return <div> 404 not found or redirect to something? </div>;
-          }
-          return (
-            <ProfileContainer
-              id={props.match.params.user_id}
-              component={component}
-            />
-          );
-        }}
-      />
-      {/* <ProfileContainer /> */}
+      <RouteContainer />
     </div>
   );
 };
---
+++ src/newApp/RouteContainer.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Route } from "react-router";
+import ProfileContainer from "./profile/general/ProfileContainer";
+import { DeveloperPage, ProjectOwnerPage, AdminPage } from "./profile/roles";
+
+const RouteContainer = () => {
+  return (
+    <>
+      <Route
+        path="/:type-id-:user_id"
+        render={props => {
+          let component = null;
+          const { type } = props.match.params;
+          if (type === "developer") {
+            component = DeveloperPage;
+          } else if (type === "project-owner") {
+            component = ProjectOwnerPage;
+          } else if (type === "admin") {
+            // redirect?
+            component = AdminPage;
+          } else {
+            return <div> 404 not found or redirect to something? </div>;
+          }
+          return (
+            <ProfileContainer
+              id={props.match.params.user_id}
+              component={component}
+            />
+          );
+        }}
+      />
+    </>
+  );
+};
+
+export default RouteContainer;
